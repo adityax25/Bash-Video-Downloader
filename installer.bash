9d5b36e27524c918efc3cac1e0949191520662e5
brendanlucas01 <69147541+brendanlucas01@users.noreply.github.com>
2022-05-12 10:25:22 +0530

    Delete installer.bash

--- installer.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl --no-check-certificate
-sudo chmod a+rx /usr/local/bin/youtube-dl
-sudo ln -s /usr/bin/python3 /usr/local/bin/python
-sudo apt-get install atomicparsley
-sudo apt install ffmpeg
